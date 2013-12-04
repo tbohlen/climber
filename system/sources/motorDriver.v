073d53780926c426c2fe0bafc19fbfc76f5615e0
Turner Bohlen <turnerbohlen@gmail.com>
2013-12-04 13:17:43 -0500

    Adds glove i/o modules

---
+++ system/sources/motorDriver.v
@@ -0,0 +1,11 @@
+//
+// File: motorDriver.v
+// Date: 12/3/2013
+// Author: Turner Bohlen
+//
+// Takes in a single bit. If the bit is 1, outputs 1. If it is 0, outputs 0.
+// This will be modified to be more sophisticated later on
+//
+
+module motorDriver(input motorState, output drive);
+    assign drive = motorState;
